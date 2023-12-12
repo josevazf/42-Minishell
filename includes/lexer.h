6a1b2b6db1868f1509de4d893558a58f4841f10a
jrocha-v <jrocha-v@student.42porto.com>
2023-12-12 18:53:46 +0000

    Added token type defines

--- includes/lexer.h
+++ includes/lexer.h
@@ -6,7 +6,7 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 09:24:19 by tiaferna          #+#    #+#             */
-/*   Updated: 2023/12/12 14:45:03 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/12/12 17:37:12 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,17 @@
 
 # include "minishell.h"
 
+# define TYPE_CMD 1
+# define TYPE_ARG 2
+# define TYPE_TEXT 3
+# define TYPE_PIPE 4
+# define TYPE_REDIR 5
+
 typedef struct s_lexer
 {
-	char	*type;
+	int		type;
 	char	*input;
+	char	*path;
 	struct s_lexer *next;
 }	t_lexer;
 
