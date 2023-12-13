37bff4d5d0bdb0c04ef2850b1aa3e00668838229
josevazf <jose.guilherme.rvf@gmail.com>
2023-12-13 12:29:14 +0000

    Updated 'cmd not found'

--- src/lexer_cmd.c
+++ src/lexer_cmd.c
@@ -6,7 +6,7 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 08:29:36 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/12/13 11:59:46 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2023/12/13 12:20:45 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,45 +62,33 @@ char	*get_cmd_path(char **envp_paths, char *cmd)
 	}
 	return (NULL);
 }
-
-void	cmd_router(char *cmd)
+/* Return the type of the command, Shell native or Builtin */
+int		cmd_router(char *cmd)
 {
-	if (ft_strcmp(cmd, "echo") == 0)
-		ft_printf("builtin: echo-> %s\n", cmd);
-	else if (ft_strcmp(cmd, "cd") == 0)
-		ft_printf("builtin: cd-> %s\n", cmd);
-	else if (ft_strcmp(cmd, "pwd") == 0)
-		ft_printf("builtin: pwd-> %s\n", cmd);
-	else if (ft_strcmp(cmd, "export") == 0)
-		ft_printf("builtin: export-> %s\n", cmd);
-	else if (ft_strcmp(cmd, "unset") == 0)
-		ft_printf("builtin: unset-> %s\n", cmd);
-	else if (ft_strcmp(cmd, "env") == 0)
-		ft_printf("builtin: env-> %s\n", cmd);
-	else if (ft_strcmp(cmd, "exit") == 0)
-		ft_printf("builtin: exit-> %s\n", cmd);
+	if (ft_strcmp(cmd, "echo") == 0 || ft_strcmp(cmd, "cd") == 0 ||
+		ft_strcmp(cmd, "pwd") == 0 || ft_strcmp(cmd, "export") == 0 ||
+		ft_strcmp(cmd, "unset") == 0 || ft_strcmp(cmd, "env") == 0 || 
+		ft_strcmp(cmd, "exit") == 0)
+		return (TYPE_B_CMD);
 	else
-		ft_printf("shell cmd (%s)\n", cmd);
+		return (TYPE_S_CMD);
 }
 
-// TO UPDATEEEEE
 void	find_cmd(t_lexer *tokens, char **envp)
 {
 	char	**paths;
 	char	*cmd_path;
-
+	
 	paths = parse_path(envp);
 	cmd_path = get_cmd_path(paths, tokens->input);
 	if (cmd_path == NULL)
 	{
-		free(tokens->input);
 		ft_free_smatrix(paths);
 		free(cmd_path);
 		ft_error(ft_strjoin("minishell: ", tokens->input), CMD_NOT_FOUND);
 	}
-	tokens->type = TYPE_CMD;
+	tokens->type = cmd_router(tokens->input);
 	tokens->path = cmd_path;
-	cmd_router(tokens->input);
 	ft_free_smatrix(paths);
 	free(cmd_path);
 }
