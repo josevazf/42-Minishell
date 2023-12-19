491ea47c6c2896b40bee061c93559d9cc871cf7d
jrocha-v <jrocha-v@student.42porto.com>
2023-12-19 12:22:09 +0000

    added tests folder

---
+++ tests/src/test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "test.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	int i = -1;
+	char **temp;
+	char *var = NULL;
+	char *content = NULL;
+	int		nb_lines;
+	
+	i = 0;
+	nb_lines = 0;
+	while (envp[++i])
+		nb_lines++;
+	i = -1;
+	while (++i < nb_lines) 
+	{
+		temp = ft_split(envp[i], '=');
+		var = ft_strdup(temp[0]);
+		content = ft_strdup(temp[1]);
+		printf("%s\t", var);
+		printf("%s\n", content);
+		ft_free_smatrix(temp);
+		free(var);
+		free(content);
+	}
+	(void)argc;
+	(void)argv;
+}
