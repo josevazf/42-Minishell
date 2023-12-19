0dbcb16b79c1f308d88a7bb2f5d834dc56497951
TiagoMartins14 <92262694+TiagoMartins14@users.noreply.github.com>
2023-12-19 12:30:49 +0000

    "quotes work in progress"

--- src/lexer_split.c
+++ src/lexer_split.c
@@ -6,26 +6,58 @@
 /*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 10:11:12 by tiaferna          #+#    #+#             */
-/*   Updated: 2023/12/19 08:48:12 by tiaferna         ###   ########.fr       */
+/*   Updated: 2023/12/19 12:17:59 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lexer.h"
 
+void	quotes_checker(char const *s)
+{
+	int	i;
+	int	s_quote;
+	int	d_quote;
+
+	i = 0;
+	s_quote = 0;
+	d_quote = 0;
+	while (s[i])
+	{
+		if (s[i] == '\'')
+		{
+			s_quote++;
+			i++;
+			while (s[i] != '\'' && s[i])
+				i++;
+			if (s[i] && s[i] == '\'')
+				s_quote++;
+		}
+		else if (s[i] == '\"')
+		{
+			d_quote++;
+			i++;
+			while (s[i] != '\"' &&s[i])
+				i++;
+			if (s[i] && s[i] == '\"')
+				d_quote++;
+		}
+		i++;
+	}
+	if (s_quote % 2 != 0 || d_quote % 2 != 0)
+		quotes_error();
+	return ;
+}
+
 void	lexer_elements_aid(char const *s, size_t *i)
 {
+	char quote;
+	
 	while (!ft_iswhitespace(s[*i]) && s[*i])
 	{
-		if (s[*i] == '\"' || s[*i] == '\'')
+		if (s[*i] == '\'' || s[*i] == '\"')
 		{
-			(*i)++;
-			while (s[*i] != '\"' && s[*i] != '\'' && s[*i])
-				(*i)++;
-			if (!s[*i])
-				quotes_error();
-			if (s[*i] == '\"' || s[*i] == '\'')
-				(*i)++;
-			while (!ft_iswhitespace(s[*i]) && s[*i])
+			quote = s[(*i)++];
+			while (s[*i] != quote && s[*i])
 				(*i)++;
 		}
 		(*i)++;
@@ -50,31 +82,35 @@ static int	lexer_elements(char const *s)
 		while (ft_iswhitespace(s[i]) && s[i])
 			i++;
 	}
+	ft_printf("returned elements : %d\n", elements); // TEST
 	return (elements);
 }
 
-static int	lexer_size_of_word(char const *s, size_t *i, char **arr)
+static int	lexer_size_of_word(char const *s, size_t *i) // REVER
 {
-	size_t	len;
-	size_t	quote;
+	int	len;
+	// size_t	ignore_quote;
+	char	quote;
 	
-	(void)arr;
 	len = 0;
-	quote = 0;
-	if (!s[*i])
+	// ignore_quote = 0;
+	while (!ft_iswhitespace(s[*i]) && s[*i])
 	{
-		// ft_free_smatrix(arr);
-		quotes_error();
+		if (s && (*s == '\'' || *s == '\"'))
+		{
+			quote = s[*i];
+			ft_printf("quote = %c\n", quote);
+			while (s[*i] && s[++(*i)] != quote)
+				len++;
+		}
+		else
+		{
+			while (s[*i] && !ft_iswhitespace(s[(*i)++]))
+				len++;
+		}
+		ft_printf("s[*i] = %c\n", s[*i]);
 	}
-	while (s[*i + len] && !ft_iswhitespace(s[*i + len]))
-	{
-		if (s[*i + len] == '\"' || s[*i + len] == '\'')
-			break ;
-		if (s[*i + len] == '\"' || s[*i + len] == '\'')
-			quote++;
-		len++;
-	}
-	return (len - quote);
+	return (len /* - ignore_quote */);
 }
 
 char	*ft_lexer_substr(char const *s, unsigned int start, size_t len)
@@ -84,7 +120,7 @@ char	*ft_lexer_substr(char const *s, unsigned int start, size_t len)
 	size_t	j;
 
 	i = 0;
-	j = 0;
+	j = 1;
 	if (ft_strlen(s) - start < len)
 		len = ft_strlen(s) - start;
 	if (ft_strlen(s) < start)
@@ -94,21 +130,25 @@ char	*ft_lexer_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	while (i < len)
 	{
-		if (s[start + j] != '\'' && s[start + j] != '\"')
-		{
-			substr[i] = s[start + j];
-			i++;
-		}
+		// if (s[start + j] != '\'' && s[start + j] != '\"')
+		// {
+		substr[i] = s[start + j];
+		i++;
+		// }
 		j++;
 	}
 	substr[i] = '\0';
 	return (substr);
 }
 
-void	create_token(char const *s, char	**arr, size_t *i, size_t *j)
+void	create_token(char const *s, char **arr, size_t *i, size_t *j)
 {
+	size_t	len;
+
+	len = 0;
 	while (s[*i])
 	{
+		ft_printf("j = %d\n", *j);
 		while (ft_iswhitespace(s[*i]))
 			(*i)++;
 		if (s[*i] == '\0')
@@ -120,14 +160,12 @@ void	create_token(char const *s, char	**arr, size_t *i, size_t *j)
 		}
 		if (s[*i])
 		{
+			len = lexer_size_of_word(s, i);
+			ft_printf("len = %d\n", len);
+			arr[*j] = ft_lexer_substr(s, *i - len - 1, len);
+			arr[*j][len] = '\0';
 			if (s[*i] == '\"' || s[*i] == '\'')
 				(*i)++;
-			arr[*j] = ft_lexer_substr(s, (*i), lexer_size_of_word(s, i, arr));
-			arr[*j][lexer_size_of_word(s, i, arr)] = '\0';
-			*i += lexer_size_of_word(s, i, arr) + 1;
-			if (s[*i] == '\"' || s[*i] == '\'')
-				(*i)++;
-			(*i)++;
 		}
 		*j += 1;
 	}
@@ -141,15 +179,14 @@ char	**lexer_split(char const *s)
 
 	i = 0;
 	j = 0;
+	quotes_checker(s);
 	if (lexer_elements(s) == 0)
 		return NULL;
-	ft_printf("elements: %d\n", lexer_elements(s) + 1); //TEST
 	arr = malloc(sizeof(char *) * (lexer_elements(s) + 1));
 	if (!arr)
 		return (NULL);
 	while (s[i])
 		create_token(s, arr, &i, &j);
 	arr[j] = NULL;
-	i = 0;
 	return (arr);
 }
