732ab651c61266871c7ceab470de04279a2aa249
jrocha-v <jrocha-v@student.42porto.com>
2024-01-02 12:03:59 +0000

    added find pipe node

--- src/parser_main.c
+++ src/parser_main.c
@@ -6,31 +6,30 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 09:06:55 by jrocha-v          #+#    #+#             */
-/*   Updated: 2023/12/29 12:03:56 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2024/01/02 11:55:15 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/parser.h"
 
-/* void	create_pipe_node(t_lexer *tokens, int type)
+/* Search Lexer list for a Pipe and return the corresponding node */
+t_lexer	*find_pipe(t_lexer *tokens)
 {
-	t_node	*node;
-	
-	
-	
-} */
+	while (tokens && (tokens->operator != PIPE))
+		tokens = tokens->next;
+	return (tokens);
+}
 
 int		parser_main(t_mshell *init)
 {
-	t_node	*parser_tree;
-	t_node	*head;
+	t_parser	*head;
+	
+	head = NULL;
 	
 	parser_tree = (t_parser *)malloc(sizeof(t_parser));
 	head = parser_tree;
 	parser_tree->left = NULL;
 	parser_tree->type = CMD;
-	while (tokens->operator != TYPE_PIPE)
-		tokens = tokens->next;
 	while (tokens)
 	{
 		parser_tree->left = NULL;
