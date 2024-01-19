e94f26736a8a5d8fceadad7cd17865f289ef0b5e
TiagoMartins14 <92262694+TiagoMartins14@users.noreply.github.com>
2024-01-19 00:14:55 +0000

    With norminette rules

--- src/export.c
+++ src/export.c
@@ -3,53 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: patatoss <patatoss@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tiago <tiago@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 11:22:25 by patatoss          #+#    #+#             */
-/*   Updated: 2024/01/04 19:11:42 by patatoss         ###   ########.fr       */
+/*   Updated: 2024/01/19 00:10:15 by tiago            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lexer.h"
-
-void	save_in_stash(t_env *node, t_env *stash)
-{
-	t_env *stash_node;
-
-	stash_node = stash;
-	if (!stash_node->var)
-		stash_node->var = ft_strdup(node->var);
-	else
-	{
-		while(stash_node->next)
-			stash_node = stash_node->next;
-		stash_node->next = (t_env *)malloc(sizeof(t_env));
-		stash_node = stash_node->next;
-		stash_node->var = NULL;
-		stash_node->content = NULL;
-		stash_node->next = NULL;
-		stash_node->var = ft_strdup(node->var);
-	}
-}
-
-int	check_stash(t_env *node, t_env *stash)
-{
-	t_env *stash_node;
-
-	stash_node = stash;
-	while (stash_node)
-	{
-		if (ft_strcmp(node->var, stash_node->var) == 0)
-			return (0);
-		stash_node = stash_node->next;
-	}
-	return (1);
-}
-
-void	export_list()
-{
-
-}
 
 void	export_new(t_mshell *init)
 {
@@ -64,9 +25,7 @@ void	export_new(t_mshell *init)
 	while (lexer_node)
 	{
 		env_node->next = (t_env *)malloc(sizeof(t_env));
-		env_node->next->var = NULL;
-		env_node->next->content = NULL;
-		env_node->next->next = NULL;
+		env_table_init(env_node->next);
 		env_node = env_node->next;
 		i = 0;
 		while (lexer_node->str[i] && lexer_node->str[i] != '=')
@@ -85,52 +44,27 @@ void	export_new(t_mshell *init)
 void	export(t_mshell *init)
 {
 	t_env	*env_node;
-	t_env	*to_print;
+	t_env	*prnt;
 	t_env	*count;
 	t_env	*stash;
 	int		flag;
-	
+
 	if (!init->lexer->next)
 	{
 		stash = (t_env *)malloc(sizeof(t_env));
-		stash->var = NULL;
-		stash->content = NULL;
-		stash->next = NULL;
-		env_node = init->env_table;
-		count = init->env_table;
-		flag = 0;
+		env_table_init(stash);
+		assign_val(&env_node, &count, &flag, init);
 		while (count)
 		{
-			to_print = env_node;
-			while (env_node)
-			{
-				if (ft_strcmp(to_print->var, env_node->var) < 0)
-					to_print = env_node;
-				env_node = env_node->next;
-			}
-			env_node = init->env_table;
-			while (env_node)
-			{
-				while (env_node && stash->var && check_stash(env_node, stash) == 0)
-					env_node = env_node->next;
-				if (!env_node)
-					break ;
-				if (ft_strcmp(to_print->var, env_node->var) > 0)
-					to_print = env_node;
-				env_node = env_node->next;
-			}
-			if (ft_strcmp("OLDPWD", to_print->var) == 0)
-				flag = 1;
-			if (ft_strcmp("OLDPWD", to_print->var) < 0 && flag == 0)
-			{
-				flag = 1;
-				ft_printf("declare -x OLDPWD\n", to_print->var, to_print->content);
-			}
-			ft_printf("declare -x %s=\"%s\"\n", to_print->var, to_print->content);
-			save_in_stash(to_print, stash);
+			sort_list(&prnt, env_node, init, stash);
+			check_oldpwd(prnt, &flag);
+			if (strcmp(prnt->var, "_") != 0)
+				ft_printf("declare -x %s=\"%s\"\n", prnt->var, prnt->content);
+			save_in_stash(prnt, stash);
 			env_node = init->env_table;
 			count = count->next;
 		}
+		free_env(stash);
 	}
 	else
 		export_new(init);
