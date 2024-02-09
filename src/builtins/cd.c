70c8c3758dc79240d8a50fd63ce31825488be1f0
TiagoMartins14 <92262694+TiagoMartins14@users.noreply.github.com>
2024-02-09 18:04:19 +0000

    Update cd.c

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -12,31 +12,88 @@
 
 #include "../includes/minishell.h"
 
-void	cd(t_mshell *init)
+char	*get_PWD(t_mshell *init)
+{
+	t_env	*node;
+
+	node = init->env_table;
+	while (node && ft_strcmp("PWD", node->var) != 0)
+		node = node->next;
+	if (!node)
+		return (NULL);
+	return (node->content);
+}
+
+char	*get_HOME(t_mshell *init)
+{
+	t_env	*node;
+
+	node = init->env_table;
+	while (node && ft_strcmp("HOME", node->var) != 0)
+		node = node->next;
+	if (!node)
+		return (NULL);
+	return (node->content);
+}
+
+void	create_OLDPWD_node(t_env *node)
+{
+	node->next = (t_env *)malloc(sizeof(t_env));
+	node->next->var = ft_strdup("OLDPWD");
+}
+
+void	cd_error_checker(t_mshell *init, t_parser *parser, int *exit_code)
+{
+	t_env	*node;
+
+	node = init->env_table;
+	if (parser->cmd_exec[2])
+	{
+		printf("bash: cd: too many arguments\n");
+		*exit_code = 1;
+		exit(1);
+	}
+	else if (parser->cmd_exec[1] == NULL && get_HOME(init) == NULL)
+	{
+		printf("bash: cd: HOME not set\n");
+		*exit_code = 1;
+		exit(1);
+	}
+}
+
+void	cd(t_mshell *init, t_parser *parser, int *exit_code)
 {
 	t_env	*node;
 	char	*old_dir;
 	char	*new_dir;
 
+	cd_error_checker(init, parser, exit_code);
 	new_dir = NULL;
 	node = init->env_table;
-	while (node && ft_strcmp("PWD", node->var) != 0)
-		node = node->next;
-	old_dir = ft_strdup(node->content);
-	free(node->content);
-	chdir(init->lexer->next->str);
-	new_dir = getcwd(NULL, 0);
-	node->content = ft_strdup(new_dir);
-	while (ft_strcmp("OLDPWD", node->var) != 0)
+	old_dir = ft_strdup(getcwd(NULL, 0));
+	if (parser->cmd_exec[1] == NULL)
+		new_dir = ft_strdup(get_HOME(init));
+	else
 	{
-		if (!node->next)
-		{
-			node->next = (t_env *)malloc(sizeof(t_env));
-			node->next->var = ft_strdup("OLDPWD");
-		}
-		node = node->next;
+		chdir(parser->cmd_exec[1]);
+		new_dir = getcwd(NULL, 0);
 	}
-	free(node->content);
-	node->content = ft_strdup(old_dir);
-	free(old_dir);
+	node->content = ft_strdup(new_dir);
+	if (get_PWD(init))
+	{
+		while (ft_strcmp("OLDPWD", node->var) != 0)
+		{
+			if (!node->next)
+				create_OLDPWD_node(node);
+			node = node->next;
+		}
+		free(node->content);
+		node->content = ft_strdup(old_dir);
+		free(old_dir);
+	}
+	else
+	{
+		while (ft_strcmp("OLDPWD", node->var) != 0)
+			node = node->next;
+	}
 }
