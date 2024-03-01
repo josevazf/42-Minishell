3f05df855e1c5dbc195c844b674ea0a491d282e3
José Vaz Francisco <jose.guilherme.rvf@gmail.com>
2024-03-01 15:43:22 +0000

    Update parser.h

--- includes/parser.h
+++ includes/parser.h
@@ -20,25 +20,28 @@
 
 // parser_main.c
 t_parser	*create_parser_node(t_mshell *init, char *cmds, t_parser *node);
-void		parser_node_push_back(t_mshell *init, t_parser **begin_list, 
-                                        char *cmds, t_parser *node);
-t_parser	*parser_node_router(t_mshell *init, char **envp_copy, t_parser *parser, char *cmds);
-void	parser_main(t_mshell *init, char **envp_copy, t_parser *parser, char *cmds);
+void		parser_node_push_back(t_mshell *init, t_parser **begin_list,	
+				char *cmds, t_parser *node);
+t_parser	*parser_node_router(t_mshell *init, char ***envp_copy,
+				t_parser *parser, char *cmds);
+void		parser_main(t_mshell *init, char ***envp_copy, t_parser *parser,
+				char *cmds);
 
 // parser_cmd.c
 int			cmd_router(char *cmd);
 char		**parse_path(char **env_path);
 char		*get_cmd_path(char **envp_paths, char *cmd);
-char	*find_cmd(char *cmd, t_mshell *init, char **envp_copy);
+char		*find_cmd(char *cmd, t_mshell *init, char ***envp_copy);
 
 // parser_utils.c
 char		*get_redirs(t_mshell *init, char *og_redirs, t_lexer **lexer);
 char		*parser_merge_split(char *og_str, char *lexer_str);
-void		free_parser_temps(char *cmds, char *redirs, char *cmd_path, char **cmd_full);
+void		free_parser_temps(char *cmds, char *redirs, char *cmd_path,
+				char **cmd_full);
 void		free_parser_vars(char **cmds, char **redirs);
 
 // parser_redirs.c
-void    	redirs_router(t_mshell *init, char *redirs);
+void		redirs_router(t_mshell *init, char *redirs);
 int			process_file(t_mshell *init, char *file_name, int file_type);
 
 // structs_init.c
