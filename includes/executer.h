783ba3be8a1d160ceb04ff046d21eb2c2177385a
José Vaz Francisco <jose.guilherme.rvf@gmail.com>
2024-03-01 15:42:51 +0000

    Update executer.h

--- includes/executer.h
+++ includes/executer.h
@@ -17,31 +17,32 @@
 
 // executer_main.c
 void	get_exit_code(int status, int *exit_code);
-void	executer_fork_router(t_mshell *init, char **env, int *exit_code, int i);
-void	executer_main(t_mshell *init, char **envp_copy, int *exit_code);
+void	executer_fork_router(t_mshell *init, char ***env, int *exit_code,
+			int i);
+void	executer_main(t_mshell *init, char ***envp_copy, int *exit_code);
 
 // executer_utils.c
 void	write_here_doc(t_mshell *init, char *eof, int *pipe_fd);
 int		process_here_doc(t_mshell *init, char *eof);
 void	get_pipes(t_mshell *init);
 char	**convert_env(t_mshell *init);
-void	executer_cmd_router(t_mshell *init, t_parser *parser_node, 
-							char **strings_env,	int *exit_code);
+void	executer_cmd_router(t_mshell *init, t_parser *parser_node,
+			char ***strings_env, int *exit_code);
 
 // executer_single_cmd.c
 int		single_cmd_notfound(t_mshell *init);
-void 	fork_single_cmd(t_mshell *init, t_parser *parser_node, char **strings_env, 
-													int *exit_code);
-void	process_single_cmd(t_mshell *init, char **strings_env, int *exit_code);
+void	fork_single_cmd(t_mshell *init, t_parser *parser_node,
+			char ***strings_env, int *exit_code);
+void	process_single_cmd(t_mshell *init, char ***strings_env, int *exit_code);
 void	single_redirs_router(t_mshell *init, t_parser *node);
 
 // executer_multi_cmds.c
 int		multi_cmd_notfound(t_mshell *init, t_parser *parser_node);
 int		**process_pipes(t_mshell *init, int **pipe_fds);
-void	process_child(t_mshell *init, int **pipe_fds, t_parser *parser_node, 
-											char **strings_env, int *exit_code);
-void	process_parent(t_mshell *init, int **pipe_fds, int *child_pids, 
-													int *exit_code);
+void	process_child(t_mshell *init, int **pipe_fds, t_parser *parser_node,
+			char ***strings_env, int *exit_code);
+void	process_parent(t_mshell *init, int **pipe_fds, int *child_pids,
+			int *exit_code);
 
 // executer_multi_redirs.c
 void	multi_check_input(t_mshell *init, t_parser *node, int i);
@@ -58,5 +59,6 @@ void	close_pipes(t_mshell *init, int **pipe_fds);
 // executer_redirs_utils.c
 int		check_red_error(char *redir_syntax);
 int		process_file(t_mshell *init, char *file_name, int file_type);
+
 
 #endif
