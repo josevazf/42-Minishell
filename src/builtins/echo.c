f5eb9d53206538ba0584ad2f828ee28f5eee6b8f
TiagoMartins14 <92262694+TiagoMartins14@users.noreply.github.com>
2024-03-29 13:43:46 +0000

    fixed leaks

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 07:57:15 by tiaferna          #+#    #+#             */
-/*   Updated: 2024/03/29 13:16:49 by tiaferna         ###   ########.fr       */
+/*   Updated: 2024/03/29 13:42:40 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,8 @@ void	echo(t_parser *parser, int i, int j, t_mshell *init)
 	flag = 0;
 	if (!parser->cmd_exec[j])
 		flag = 0;
-	while (parser->cmd_exec[j][0] == '-' && parser->cmd_exec[j][1] == 'n')
+	while (parser->cmd_exec[j] && parser->cmd_exec[j][0] == '-' \
+							&& parser->cmd_exec[j][1] == 'n')
 	{
 		i = 2;
 		while (parser->cmd_exec[j][i] == 'n')
