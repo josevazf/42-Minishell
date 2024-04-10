f984c3eda90bcf216523cc0dddf9fb04e63b0739
Tiago Martins <tiago@tiaferna.lan>
2024-04-10 19:33:03 +0100

    fixed echo with no args

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 07:57:15 by tiaferna          #+#    #+#             */
-/*   Updated: 2024/04/10 18:23:46 by tiaferna         ###   ########.fr       */
+/*   Updated: 2024/04/10 19:31:15 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ void	print_str(t_lexer *lex_nd, int fd)
 {
 	while (lex_nd)
 	{
+		if ((ft_strcmp(lex_nd->str, "<<") == 0 || \
+		ft_strcmp(lex_nd->str, ">>") == 0 || \
+		ft_strcmp(lex_nd->str, "<") == 0 || \
+		ft_strcmp(lex_nd->str, ">") == 0) && lex_nd->true_sign == true)
+			break ;
 		write(fd, lex_nd->str, ft_strlen(lex_nd->str));
 		if (lex_nd->next)
 			write(fd, " ", 1);
@@ -28,9 +33,10 @@ void	echo(t_mshell *init, t_parser *parser, t_lexer *lex_nd, int i)
 	int		flag;
 
 	flag = 0;
-	if (!lex_nd->str)
+	lex_nd = lex_nd->next;
+	if (!lex_nd || !lex_nd->str)
 		flag = 0;
-	while (lex_nd->str && lex_nd->str[0] == '-' \
+	while (lex_nd && lex_nd->str && lex_nd->str[0] == '-' \
 							&& lex_nd->str[1] == 'n')
 	{
 		i = 2;
@@ -40,14 +46,12 @@ void	echo(t_mshell *init, t_parser *parser, t_lexer *lex_nd, int i)
 			flag = 1;
 		else
 			break ;
+		lex_nd = lex_nd->next;
 	}
-	lex_nd = lex_nd->next;
-	if (lex_nd->str)
+	if (lex_nd && lex_nd->str)
 		print_str(lex_nd, parser->output);
-	if (flag == 0)
+	if (flag == 0 || !lex_nd)
 		write(parser->output, "\n", 1);
 	delete_lists(init);
 	exit (0);
 }
-
-// MUDAR PARA CORRER ATRAVÉS DO LEXER
